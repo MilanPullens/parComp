e0c0dd15f30125d2a42745478322b9954a122929
MilanPullens <milan141200@hotmail.nl>
2023-05-26 14:44:24 +0200

    working mpi

--- src/mpi/stencil.c
+++ src/mpi/stencil.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
-
+#include <sched.h>
 #include <immintrin.h>
 
 #define REAL double
@@ -25,69 +25,81 @@ void Stencil(REAL *in, REAL *out, size_t n, int iterations)
             in = out;
             out = temp;
         }
-    }
+    }    
 }
+
 
 int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     if (argc != 3) {
-        if (rank == 0)
+        if (world_rank == 0) {
             printf("Please specify 2 arguments (n, iterations).\n");
+        }
         MPI_Finalize();
         return EXIT_FAILURE;
     }
 
-    size_t n = atoll(argv[1]);
+    int n = atoi(argv[1]);
     int iterations = atoi(argv[2]);
 
-    // Determine the local size of the data for each process
-    size_t local_n = n / size;
-    if (rank < n % size)
+    size_t local_n = n / world_size;
+    if (world_rank < n % world_size)
         local_n++;
 
-    // Allocate memory for the local portion of the data
-    REAL *local_in = calloc(local_n, sizeof(REAL));
+    // Allocate memory for local data
+    REAL *local_in = malloc(local_n * sizeof(REAL));
     REAL *local_out = malloc(local_n * sizeof(REAL));
 
-    // Initialize the local portion of the data
-    local_in[0] = 100;
-    local_in[local_n - 1] = 1000;
+    // Allocate memory for the global data on process 0
+    REAL *global_in = NULL;
+    if (world_rank == 0)
+        global_in = malloc(n * sizeof(REAL));
 
+    // Initialize the global data on process 0
+    if (world_rank == 0) {
+        global_in[0] = 100;
+        global_in[n - 1] = 1000;
+    }
+
+    // Scatter the global input data to all processes
+    MPI_Scatter(global_in, local_n, MPI_DOUBLE, local_in, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Perform stencil computation
     double start_time = MPI_Wtime();
     Stencil(local_in, local_out, local_n, iterations);
     double end_time = MPI_Wtime();
 
-    // Compute the local duration
-    double duration = end_time - start_time;
+    double duration = end_time - start_time;   
 
-    // Gather the results from all processes to process 0
+
     REAL *result = NULL;
-    if (rank == 0) {
+    if (world_rank == 0)
         result = malloc(n * sizeof(REAL));
-    }
+
     MPI_Gather(local_out, local_n, MPI_DOUBLE, result, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
+
+
+    if (world_rank == 0) {
         // Compute the overall performance
-        double total_duration;
-        MPI_Reduce(&duration, &total_duration, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
         // Compute the overall GFLOPS/s
-        double gflopsS = 5.0 * (n - 2) * iterations / 1e9 / total_duration;
+        double gflopsS = 5.0 * (n - 2) * iterations / 1e9 / duration;
 
-        printf("This took %lf seconds, or %lf GFLOPS/s\n", total_duration, gflopsS);
+        printf("%lf", gflopsS);
 
         free(result);
     }
 
     free(local_in);
     free(local_out);
+    free(global_in);
 
     MPI_Finalize();
 
